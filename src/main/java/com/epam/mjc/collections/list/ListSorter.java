69ef103ad3526cd0d0bdd4bcb69b7e4ab203bed8
Vitali Kalaskou <kolosokvit@gmail.com>
2023-10-06 09:35:38 +0300

    Complete List tasks

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator listComparator = new ListComparator();
+        sourceList.sort(listComparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int numberA = Integer.parseInt(a);
+        int numberB = Integer.parseInt(b);
+        if (numberA * numberA == numberB * numberB) {
+            return numberA - numberB;
+        } else {
+            return (numberA * numberA - numberB * numberB);
+        }
     }
 }
